64a2c98f2cd04de570ee8a32eee40b65919b7572
Prasanta <prasantadutta976@gmail.co>
2023-01-14 15:00:04 +0000

    add new contact

--- app/src/main/java/com/example/androidcrud/addContactActivity.java
+++ app/src/main/java/com/example/androidcrud/addContactActivity.java
@@ -4,17 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class addContactActivity extends AppCompatActivity {
+    EditText contactName, contactPhone;
+    Button addNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        contactName = findViewById(R.id.contactNameAdd);
+        contactPhone = findViewById(R.id.editTextPhone);
+
+        addNew = findViewById(R.id.addContactSubmitBtn);
     }
 
     public void addNewContact(View view) {
-        Toast.makeText(this, "Contact Added!!", Toast.LENGTH_SHORT).show();
+        String name  = contactName.toString();
+        String number = contactPhone.toString();
+
+        if (name == null && number == null)
+            Toast.makeText(this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
+        else
+           Toast.makeText(this, "Contact Added!!", Toast.LENGTH_SHORT).show();
     }
 }
