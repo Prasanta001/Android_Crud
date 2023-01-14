ef8184201c32843719a4604269b330146a3efa31
Prasanta <prasantadutta976@gmail.co>
2023-01-14 14:24:23 +0000

    add new contact

--- app/src/main/java/com/example/androidcrud/MainActivity.java
+++ app/src/main/java/com/example/androidcrud/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.androidcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addContact, viewContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addContact = findViewById(R.id.addContactBtn);
+        viewContact = findViewById(R.id.viewContactBtn);
     }
+
 }
